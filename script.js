383e104da9f6d3b76a5515fcba70ee5719b7d4b8
sali1993 <salahudeenali28@gmail.com>
2021-05-05 16:02:11 -0400

    start button clickable & instruction prompt

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("connected!")
+//variables
+var button = document.querySelectorAll(".game-button");
+var startButton = document.querySelector(".start-button");
+var playerInstructions = document.querySelector(".playerInstructions")
+let compSequence =[];
+let playerSequence =[];
+
+
+//Functions
+function startGame(){
+    startButton.classList.add("hidden")
+    playerInstructions.classList.remove("hidden");
+    playerInstructions.innerText="Wait for computer";
+    
+}
+
+
+//Event Listeners
+startButton.addEventListener("click", startGame)
