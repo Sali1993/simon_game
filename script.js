a381b1f8a5e0658f1de8130776330083ae2a93c8
sali1993 <salahudeenali28@gmail.com>
2021-05-05 19:34:06 -0400

    basic page outline and start button funtional

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 //variables
 var button = document.querySelectorAll(".game-button");
 var startButton = document.querySelector(".start-button");
-var playerInstructions = document.querySelector(".playerInstructions")
+var playerInstructions = document.querySelector(".playerInstructions");
 let compSequence =[];
 let playerSequence =[];
+let level = 0;
 
 
 //Functions
+function nextRound(){
+    level +=1
+    const newSequence =[...compSequence]//copies computer sequence and stores it in order to add to it
+}
+
+function nextStep() {
+    const colors = ['red', 'green', 'blue', 'yellow'];
+    const random = colors[Math.floor(Math.random() * tiles.length)];
+
+    return random;
+}
+
+
 function startGame(){
-    startButton.classList.add("hidden")
+    startButton.classList.add("hidden")//hides start button and begins game promts
     playerInstructions.classList.remove("hidden");
     playerInstructions.innerText="Wait for computer";
     
