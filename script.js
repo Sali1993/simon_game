3a87b08aed0e5101177d878a5f6f5515f9e92620
sali1993 <salahudeenali28@gmail.com>
2021-05-08 10:15:13 -0400

    level tally added

--- script.js
+++ script.js
@@ -2,12 +2,19 @@
 var button = document.querySelectorAll(".game-button");
 var startButton = document.querySelector(".start-button");
 var playerInstructions = document.querySelector(".playerInstructions");
+var levelTally = document.querySelector(".level-tally")
+var gameBoard = document.querySelector(".button-container");
 let compSequence =[];
 let playerSequence =[];
 let level = 0;
 
 
 //Functions
+
+function yourTurn(level){
+gameBoard.classList.remove("unclickable")//activates buttons
+playerInstructions.innerText=`Your go! ${level} click${level>1?'s':''}`//changes player prompt, adds "s" after level 1
+}
 
 function activeColor(color){
     const currentColor = document.querySelector(`[data-color="${color}"]`)
@@ -17,8 +24,6 @@ function activeColor(color){
     setTimeout(()=>{
         currentColor.classList.remove("activated")
     },300);//activates and plays sound the button for 3 seconds
-
-
 }
 
 function play(newSequence){
@@ -30,10 +35,17 @@ newSequence.forEach((color,index)=>{
 }
 
 function nextRound(){
-    level +=1
-    const newSequence =[...compSequence]//copies computer sequence and stores it in order to add to it
-    newSequence.push(nextStep()) //adds random value to existing comuter sequence
+    level +=1;
+    gameBoard.classList.add("unclickable");
+    playerInstructions.innerText="Wait for computer";
+    levelTally.innerText = `Level ${level} of 25`;
+    const newSequence =[...compSequence];//copies computer sequence and stores it in order to add to it
+    newSequence.push(nextStep()); //adds random value to existing comuter sequence
     play(newSequence);
+    sequence =[...newSequence];
+    setTimeout(()=>{
+        yourTurn(level);
+    }, level * 500 + 1000);//delay to start your turn after the computer squence ends
 }
 
 function nextStep() {
@@ -47,8 +59,9 @@ function startGame(){
     startButton.classList.add("hidden")//hides start button and begins game promts
     playerInstructions.classList.remove("hidden");
     playerInstructions.innerText="Wait for computer";
+    levelTally.classList.remove("hidden")
     nextRound();
-    
+    levelTally.innerText = `Level ${level} of 25`
 }
 
 
