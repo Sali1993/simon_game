a92fb9406b77e55de616fadb84a05c889d4afe2e
sali1993 <salahudeenali28@gmail.com>
2021-05-08 08:59:12 -0400

    start button activates game

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function activeColor(color){
     const currentColor = document.querySelector(`[data-color="${color}"]`)
     const sound = document.querySelector(`[data-sound="${color}"]`)
     currentColor.classList.add("activated");
-    sound.play();
+    sound.play();//.play plays sound
     setTimeout(()=>{
         currentColor.classList.remove("activated")
     },300);//activates and plays sound the button for 3 seconds
@@ -21,15 +21,24 @@ function activeColor(color){
 
 }
 
+function play(newSequence){
+newSequence.forEach((color,index)=>{
+    setTimeout(()=>{
+        activeColor(color);
+    },(index+1)*500)
+});//for each value in sequence array delay (5 seconds * the index) between values
+}
+
 function nextRound(){
     level +=1
     const newSequence =[...compSequence]//copies computer sequence and stores it in order to add to it
     newSequence.push(nextStep()) //adds random value to existing comuter sequence
+    play(newSequence);
 }
 
 function nextStep() {
     const colors = ['red', 'green', 'blue', 'yellow'];
-    const random = colors[Math.floor(Math.random() * tiles.length)];
+    const random = colors[Math.floor(Math.random() * colors.length)];
     return random;
 }
 
@@ -38,6 +47,7 @@ function startGame(){
     startButton.classList.add("hidden")//hides start button and begins game promts
     playerInstructions.classList.remove("hidden");
     playerInstructions.innerText="Wait for computer";
+    nextRound();
     
 }
 
