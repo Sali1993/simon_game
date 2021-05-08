0caf24ef7d113bda4711f0a42178e26647654dad
sali1993 <salahudeenali28@gmail.com>
2021-05-08 11:16:32 -0400

    Game partially functional

--- script.js
+++ script.js
@@ -8,53 +8,16 @@ let compSequence =[];
 let playerSequence =[];
 let level = 0;
 
+//Event Listeners
+startButton.addEventListener("click", startGame);
+
+gameBoard.addEventListener("click", event => {
+    const { color } = event.target.dataset;
+    if (color) gamePlay(color)
+});
+
 
 //Functions
-
-function yourTurn(level){
-gameBoard.classList.remove("unclickable")//activates buttons
-playerInstructions.innerText=`Your go! ${level} click${level>1?'s':''}`//changes player prompt, adds "s" after level 1
-}
-
-function activeColor(color){
-    const currentColor = document.querySelector(`[data-color="${color}"]`)
-    const sound = document.querySelector(`[data-sound="${color}"]`)
-    currentColor.classList.add("activated");
-    sound.play();//.play plays sound
-    setTimeout(()=>{
-        currentColor.classList.remove("activated")
-    },300);//activates and plays sound the button for 3 seconds
-}
-
-function play(newSequence){
-newSequence.forEach((color,index)=>{
-    setTimeout(()=>{
-        activeColor(color);
-    },(index+1)*500)
-});//for each value in sequence array delay (5 seconds * the index) between values
-}
-
-function nextRound(){
-    level +=1;
-    gameBoard.classList.add("unclickable");
-    playerInstructions.innerText="Wait for computer";
-    levelTally.innerText = `Level ${level} of 25`;
-    const newSequence =[...compSequence];//copies computer sequence and stores it in order to add to it
-    newSequence.push(nextStep()); //adds random value to existing comuter sequence
-    play(newSequence);
-    sequence =[...newSequence];
-    setTimeout(()=>{
-        yourTurn(level);
-    }, level * 500 + 1000);//delay to start your turn after the computer squence ends
-}
-
-function nextStep() {
-    const colors = ['red', 'green', 'blue', 'yellow'];
-    const random = colors[Math.floor(Math.random() * colors.length)];
-    return random;
-}
-
-
 function startGame(){
     startButton.classList.add("hidden")//hides start button and begins game promts
     playerInstructions.classList.remove("hidden");
@@ -64,6 +27,62 @@ function startGame(){
     levelTally.innerText = `Level ${level} of 25`
 }
 
+function randomizer() {
+    const colors = ['red', 'green', 'blue', 'yellow'];
+    const random = colors[Math.floor(Math.random() * colors.length)];
+    return random;
+}
 
-//Event Listeners
-startButton.addEventListener("click", startGame)
+function gamePlay(color){
+    const index = playerSequence.push(color)-1;
+    const sound = document.querySelector(`[data-sound="${color}"]`);
+    sound.play();
+    const remainingTaps = sequence.length - playerSequence.length;
+    if(playerSequence.length===sequence.length){
+        playerSequence = [];
+        playerInstructions.innerText="Dope! Time to step it up!";
+        setTimeout(()=>{
+            nextRound();
+        },1000);
+        return
+    }
+    playerInstructions = `Your Go! ${remainingTaps} click${remainingTaps>1?"s":""}`
+}
+
+function nextRound() {
+    level += 1;
+    gameBoard.classList.add("unclickable");
+    playerInstructions.innerText = "Wait for computer";
+    levelTally.innerText = `Level ${level} of 25`;
+    const newSequence = [...compSequence];//copies computer sequence and stores it in order to add to it
+    newSequence.push(randomizer()); //adds random value to existing comuter sequence
+    play(newSequence);
+    sequence = [...newSequence];
+    setTimeout(() => {
+        yourTurn(level);
+    }, level * 500 + 1000);//delay to start your turn after the computer squence ends
+}
+
+function yourTurn(level) {
+    gameBoard.classList.remove("unclickable")//activates buttons
+    playerInstructions.innerText = `Your go! ${level} click${level > 1 ? 's' : ''}`//changes player prompt, adds "s" after level 1
+}
+
+function play(newSequence) {
+    newSequence.forEach((color, index) => {
+        setTimeout(() => {
+            activeColor(color);
+        }, (index + 1) * 500)
+    });//for each value in sequence array delay (5 seconds * the index) between values
+}
+
+function activeColor(color) {
+    const currentColor = document.querySelector(`[data-color="${color}"]`)
+    const sound = document.querySelector(`[data-sound="${color}"]`)
+    currentColor.classList.add("activated");
+    sound.play();//.play plays sound
+    setTimeout(() => {
+        currentColor.classList.remove("activated")
+    }, 300);//activates and plays sound the button for 3 seconds
+}
+
