c6c9abca3e693d41bb0407c7a46bb7a58128fad9
sali1993 <salahudeenali28@gmail.com>
2021-05-11 18:14:52 -0400

    added more functionality but broke activeColor function

--- script.js
+++ script.js
@@ -1,14 +1,138 @@
 //variables
-var button = document.querySelectorAll(".game-button");
+var optionButtons= document.querySelector(".optionButtons")
+var pickChainz= document.querySelector(".pickChainz");
+var chainzButton= document.querySelector(".chainz")
+var pickChance = document.querySelector(".pickChance");
+var chanceButton= document.querySelector(".chance")
+var pickDmx = document.querySelector(".pickDmx");
+var dmxButton= document.querySelector(".dmx")
+var pickGucci = document.querySelector(".pickGucci");
+var gucciButton = document.querySelector(".gucci")
+var pickDrake = document.querySelector(".pickDrake");
+var drakeButton = document.querySelector(".drake")
+var pickJon = document.querySelector(".pickJon");
+var jonButton = document.querySelector(".jon")
+var pickNicki = document.querySelector(".pickNicki");
+var nickiButton = document.querySelector(".nicki")
+var pickBiggie = document.querySelector(".pickBiggie");
+var bigButton = document.querySelector(".biggie")
+var pickRoss = document.querySelector(".pickRoss");
+var rossButton = document.querySelector(".ross")
+var pickSoulja = document.querySelector(".pickSoulja");
+var souljaButton = document.querySelector(".soulja")
 var startButton = document.querySelector(".start-button");
 var playerInstructions = document.querySelector(".playerInstructions");
 var levelTally = document.querySelector(".level-tally")
 var gameBoard = document.querySelector(".button-container");
+var colors = [];
 let compSequence =[];
 let playerSequence =[];
+let pickedEmcees =[]
 let level = 0;
 
+
 //Event Listeners
+
+pickChainz.addEventListener("click", function() {
+    chainzButton.classList.toggle("hidden")
+    colors.push(chainzButton.getAttribute("data-color"))
+    console.log(colors)
+    if(colors.length===4){
+        startButton.classList.remove("hidden")
+        playerInstructions.innerText="Lets get it poppin!"
+    }
+})
+
+pickChance.addEventListener("click", function () {
+    chanceButton.classList.toggle("hidden")
+    colors.push(chanceButton.getAttribute("data-color"))
+    console.log(colors)
+    if (colors.length === 4) {
+        startButton.classList.remove("hidden")
+        playerInstructions.innerText="Lets get it poppin!"
+    }
+})
+
+pickDmx.addEventListener("click", function () {
+    dmxButton.classList.toggle("hidden")
+    colors.push(dmxButton.getAttribute("data-color"))
+    console.log(colors)
+    if (colors.length === 4) {
+        startButton.classList.remove("hidden")
+        playerInstructions.innerText="Lets get it poppin!"
+    }
+})
+
+pickGucci.addEventListener("click", function () {
+    gucciButton.classList.toggle("hidden")
+    colors.push(gucciButton.getAttribute("data-color"))
+    console.log(colors)
+    if (colors.length === 4) {
+        startButton.classList.remove("hidden")
+        playerInstructions.innerText="Lets get it poppin!"
+    }
+})
+
+pickDrake.addEventListener("click", function () {
+    drakeButton.classList.toggle("hidden")
+    colors.push(drakeButton.getAttribute("data-color"))
+    console.log(colors)
+    if (colors.length === 4) {
+        startButton.classList.remove("hidden")
+        playerInstructions.innerText="Lets get it poppin!"
+    }
+})
+
+pickJon.addEventListener("click", function () {
+    jonButton.classList.toggle("hidden")
+    colors.push(jonButton.getAttribute("data-color"))
+    console.log(colors)
+    if (colors.length === 4) {
+        startButton.classList.remove("hidden")
+        playerInstructions.innerText="Lets get it poppin!"
+    }
+})
+
+pickNicki.addEventListener("click", function () {
+    nickiButton.classList.toggle("hidden")
+    colors.push(nickiButton.getAttribute("data-color"))
+    console.log(colors)
+    if (colors.length === 4) {
+        startButton.classList.remove("hidden")
+        playerInstructions.innerText="Lets get it poppin!"
+    }
+})
+
+pickBiggie.addEventListener("click", function () {
+    bigButton.classList.toggle("hidden")
+    colors.push(bigButton.getAttribute("data-color"))
+    console.log(colors)
+    if (colors.length === 4) {
+        startButton.classList.remove("hidden")
+        playerInstructions.innerText="Lets get it poppin!"
+    }
+})
+
+pickRoss.addEventListener("click", function () {
+    rossButton.classList.toggle("hidden")
+    colors.push(rossButton.getAttribute("data-color"))
+    console.log(colors)
+    if (colors.length === 4) {
+        startButton.classList.remove("hidden")
+        playerInstructions.innerText="Lets get it poppin!"
+    }
+})
+
+pickSoulja.addEventListener("click", function () {
+    souljaButton.classList.toggle("hidden")
+    colors.push(souljaButton.getAttribute("data-color"))
+    console.log(colors)
+    if (colors.length === 4) {
+        startButton.classList.remove("hidden")
+        playerInstructions.innerText="Lets get it poppin!"
+    }
+})
+
 startButton.addEventListener("click", startGame);
 
 gameBoard.addEventListener("click", event => {
@@ -18,17 +142,19 @@ gameBoard.addEventListener("click", event => {
 
 
 //Functions
+
+
 function startGame(){
     startButton.classList.add("hidden")//hides start button and begins game promts
-    playerInstructions.classList.remove("hidden");
+    // playerInstructions.classList.remove("hidden");
     playerInstructions.innerText="Wait for computer";
+    optionButtons.classList.add("hidden")
     levelTally.classList.remove("hidden")
     nextRound();
     levelTally.innerText = `Level ${level} of 25`
 }
 
 function randomizer() {
-    const colors = ['red', 'green', 'blue', 'yellow'];
     const random = colors[Math.floor(Math.random() * colors.length)];
     return random;
 }
@@ -85,7 +211,7 @@ function play(newSequence) {
 }
 
 function activeColor(color) {
-    const currentColor = document.querySelector(`[data-color="${color}"]`)
+    const currentColor = document.querySelector(`[data-color="${color}]"`)
     const sound = document.querySelector(`[data-sound="${color}"]`)
     currentColor.classList.add("activated");
     sound.play();//.play plays sound
@@ -94,14 +220,18 @@ function activeColor(color) {
     }, 300);//activates and plays sound the button for 3 seconds
 }
 
+
 function reset(text){
     alert(text);
     compSequence =[];
     playerSequence =[];
+    colors=[];
+    console.log(colors)
     level=0;
-    startButton.classList.remove("hidden")
-    playerInstructions.classList.add("hidden")
+    startButton.classList.add("hidden")
+    optionButtons.classList.remove("hidden")
+    playerInstructions.innerText=""
     levelTally.classList.add("hidden")
-    gameBoard.classList.add("disabled")
+    // gameBoard.classList.add("hidden")
+   
 }
-
